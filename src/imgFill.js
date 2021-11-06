a1088a906809b07c16ad6e93978bd8e877b48752
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2021-11-06 01:47:51 +0100

    Gallery Responsivity

--- src/imgFill.js
+++ src/imgFill.js
@@ -3,7 +3,7 @@ const count = 21
 
 for (let index = 0; index < 21; index++) {
     imgPlace.innerHTML += `
-        <div class="flex flex-wrap w-1/3">
+        <div class="flex flex-wrap">
               <div class="w-full p-1 md:p-2">
                   <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
                       src="./imgs/${index + 1}.webp">
